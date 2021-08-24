1ca9b360baf7f38777b256f42513a005f3be09f7
Meeken1998 <meeken1998@gmail.com>
2021-08-24 15:39:14 +0800

    fix: interface namespace conflicts

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,94 @@
-import { SearchOptions, FetchSearch, FetchSubmission, BrowseOptions, FetchBrowse, FetchAuthor, FetchWatchingList, FetchGallery, FetchScraps, FetchHome, FetchMyWatchingList, SubmissionsOptions, FetchSubmissions } from './Request';
-import { ParseFigures, ParseSubmission, ParseAuthor, ParseWatchingList, ParseMyWatchingList, ParseScrapsPaging, ParseGalleryPaging, ParseSubmissionsPaging, ParseBrowsePaging, ParseSearchPaging } from './Parser';
-import { Author, PagingResults, Result, Submission } from './interfaces';
+import {
+  SearchOptions,
+  FetchSearch,
+  FetchSubmission,
+  BrowseOptions,
+  FetchBrowse,
+  FetchAuthor,
+  FetchWatchingList,
+  FetchGallery,
+  FetchScraps,
+  FetchHome,
+  FetchMyWatchingList,
+  SubmissionsOptions,
+  FetchSubmissions,
+} from "./Request"
+import {
+  ParseFigures,
+  ParseSubmission,
+  ParseAuthor,
+  ParseWatchingList,
+  ParseMyWatchingList,
+  ParseScrapsPaging,
+  ParseGalleryPaging,
+  ParseSubmissionsPaging,
+  ParseBrowsePaging,
+  ParseSearchPaging,
+} from "./Parser"
+import { IAuthor, IPagingResults, IResult, ISubmission } from "./interfaces"
 
-export * from './Enums';
-export * from './interfaces';
+export * from "./Enums"
+export * from "./interfaces"
 
-export { Login, SetProxy } from './Request';
+export { Login, SetProxy } from "./Request"
 
 /**
  * Get results from search page
  * @param query search query
  * @param options search options
  */
-export async function Search(query: string, options?: SearchOptions): Promise<PagingResults> {
-	const body = await FetchSearch(query, options);
-	const results = ParseFigures(body);
-	return ParseSearchPaging(body, results, query, options);
+export async function Search(
+  query: string,
+  options?: SearchOptions
+): Promise<IPagingResults> {
+  const body = await FetchSearch(query, options)
+  const results = ParseFigures(body)
+  return ParseSearchPaging(body, results, query, options)
 }
 
 /**
  * Get results from browse page
  * @param options browse options
  */
-export async function Browse(options?: BrowseOptions): Promise<PagingResults> {
-	const body = await FetchBrowse(options);
-	const results = ParseFigures(body);
-	return ParseBrowsePaging(body, results, options);
+export async function Browse(options?: BrowseOptions): Promise<IPagingResults> {
+  const body = await FetchBrowse(options)
+  const results = ParseFigures(body)
+  return ParseBrowsePaging(body, results, options)
 }
 
 /**
  * Get results from submissions timeline page
  * @param options submissions options
  */
-export async function Submissions(options?: SubmissionsOptions): Promise<PagingResults> {
-	const body = await FetchSubmissions(options);
-	const results = ParseFigures(body);
-	return ParseSubmissionsPaging(body, results);
+export async function Submissions(
+  options?: SubmissionsOptions
+): Promise<IPagingResults> {
+  const body = await FetchSubmissions(options)
+  const results = ParseFigures(body)
+  return ParseSubmissionsPaging(body, results)
 }
 
 /**
  * Get submission's info by pass submission id
  * @param id submission id
  */
-export async function Submission(id: string): Promise<Submission> {
-	return ParseSubmission(await FetchSubmission(id), id);
+export async function Submission(id: string): Promise<ISubmission> {
+  return ParseSubmission(await FetchSubmission(id), id)
 }
 
 /**
  * Get the current logged in user
  */
-export async function User(): Promise<Author> {
-	return ParseAuthor(await FetchHome());
+export async function User(): Promise<IAuthor> {
+  return ParseAuthor(await FetchHome())
 }
 
 /**
  * Get author's info by pass author id
  * @param id author id
  */
-export async function Author(id: string): Promise<Author> {
-	return ParseAuthor(await FetchAuthor(id));
+export async function Author(id: string): Promise<IAuthor> {
+  return ParseAuthor(await FetchAuthor(id))
 }
 
 /**
@@ -66,10 +96,14 @@ export async function Author(id: string): Promise<Author> {
  * @param id author id
  * @param page page number
  */
-export async function Gallery(id: string, page: number, perpage: number = 72): Promise<PagingResults> {
-	const body = await FetchGallery(id, page, perpage);
-	const results = ParseFigures(body);
-	return ParseGalleryPaging(body, results, perpage);
+export async function Gallery(
+  id: string,
+  page: number,
+  perpage: number = 72
+): Promise<IPagingResults> {
+  const body = await FetchGallery(id, page, perpage)
+  const results = ParseFigures(body)
+  return ParseGalleryPaging(body, results, perpage)
 }
 
 /**
@@ -77,10 +111,14 @@ export async function Gallery(id: string, page: number, perpage: number = 72): P
  * @param id author id
  * @param page page number
  */
-export async function Scraps(id: string, page: number, perpage: number = 72): Promise<PagingResults> {
-	const body = await FetchScraps(id, page, perpage);
-	const results = ParseFigures(body);
-	return ParseScrapsPaging(body, results, perpage);
+export async function Scraps(
+  id: string,
+  page: number,
+  perpage: number = 72
+): Promise<IPagingResults> {
+  const body = await FetchScraps(id, page, perpage)
+  const results = ParseFigures(body)
+  return ParseScrapsPaging(body, results, perpage)
 }
 
 /**
@@ -88,19 +126,19 @@ export async function Scraps(id: string, page: number, perpage: number = 72): Pr
  * result don't has avatar
  * @param id author id
  */
-export async function WatchingList(id: string): Promise<Author[]> {
-	let result: Author[] = [];
-	let page = 1;
+export async function WatchingList(id: string): Promise<IAuthor[]> {
+  let result: IAuthor[] = []
+  let page = 1
 
-	while (true) {
-		const users = ParseWatchingList(await FetchWatchingList(id, page++));
-		result = [...result, ...users];
-		if (users.length === 0 || users.length < 200) {
-			break;
-		}
-	}
+  while (true) {
+    const users = ParseWatchingList(await FetchWatchingList(id, page++))
+    result = [...result, ...users]
+    if (users.length === 0 || users.length < 200) {
+      break
+    }
+  }
 
-	return result;
+  return result
 }
 
 /**
@@ -108,17 +146,17 @@ export async function WatchingList(id: string): Promise<Author[]> {
  * this can only use after login
  * result has avatar
  */
-export async function MyWatchingList(): Promise<Author[]> {
-	let result: Author[] = [];
-	let page = 1;
+export async function MyWatchingList(): Promise<IAuthor[]> {
+  let result: IAuthor[] = []
+  let page = 1
 
-	while (true) {
-		const users = ParseMyWatchingList(await FetchMyWatchingList(page++));
-		result = [...result, ...users];
-		if (users.length === 0 || users.length < 64) {
-			break;
-		}
-	}
+  while (true) {
+    const users = ParseMyWatchingList(await FetchMyWatchingList(page++))
+    result = [...result, ...users]
+    if (users.length === 0 || users.length < 64) {
+      break
+    }
+  }
 
-	return result;
+  return result
 }
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,66 +1,66 @@
-import { SubmissionType, Species, Category, Gender, Rating } from './Enums';
+import { SubmissionType, Species, Category, Gender, Rating } from "./Enums"
 
-export interface Author {
-	id: string,
-	name: string,
-	url: string,
-	avatar?: string,
-	shinies?: boolean,
-	stats?: {
-		views: number,
-		submissions: number,
-		favs: number,
+export interface IAuthor {
+  id: string
+  name: string
+  url: string
+  avatar?: string
+  shinies?: boolean
+  stats?: {
+    views: number
+    submissions: number
+    favs: number
 
-		commentsEarned: number,
-		commentsMade: number,
-		journals: number,
-	}
+    commentsEarned: number
+    commentsMade: number
+    journals: number
+  }
 }
 
-export interface Result {
-	type: SubmissionType,
-	id: string,
-	title: string,
-	url: string,
-	rating: Rating,
-	thumb: {
-		icon: string,
-		tiny: string,
-		small: string,
-		medium: string,
-		large: string
-	},
-	author: Author,
-	getSubmission(): Promise<Submission | null>
-};
-
-export interface PagingResults extends Array<Result> {
-	prevLink?: string,
-	nextLink?: string,
-	prev?: () => Promise<PagingResults>,
-	next?: () => Promise<PagingResults>
+export interface IResult {
+  type: SubmissionType
+  id: string
+  title: string
+  url: string
+  rating: Rating
+  thumb: {
+    icon: string
+    tiny: string
+    small: string
+    medium: string
+    large: string
+  }
+  author: IAuthor
+  getSubmission(): Promise<ISubmission | null>
 }
 
-export interface Submission {
-	id: string,
-	url: string,
-	title: string,
-	posted: number,
-	rating: Rating,
-	favLink: string,
-	author: Author,
-	content: {
-		category: Category,
-		species: Species,
-		gender: Gender
-	},
-	stats: {
-		favorites: Number,
-		comments: Number,
-		views: Number
-	},
-	downloadUrl: string,
-	previewUrl?: string,
-	keywords: string[],
-	fave(): Promise<void>
-};
+export interface IPagingResults extends Array<IResult> {
+  prevLink?: string
+  nextLink?: string
+  prev?: () => Promise<IPagingResults>
+  next?: () => Promise<IPagingResults>
+}
+
+export interface ISubmission {
+  id: string
+  url: string
+  title: string
+  posted: number
+  rating: Rating
+  favLink: string
+  author: IAuthor
+  content: {
+    category: Category
+    species: Species
+    gender: Gender
+  }
+  stats: {
+    favorites: Number
+    comments: Number
+    views: Number
+  }
+  downloadUrl: string
+  previewUrl?: string
+  keywords: string[]
+  fave(): Promise<void>
+}

2fa66688baf3f3a2ca93942221d66e62c54c2ab7
su <recallsufuture@gmail.com>
2025-01-16 11:13:53 +0800

    fix: format and fix unit test

--- tests/watchAuthor.test.ts
+++ tests/watchAuthor.test.ts
@@ -2,7 +2,7 @@ import { Login, Author, watchAuthor, unwatchAuthor, User } from "../src/index";
 import * as config from "./jest.config";
 const { cookieA, cookieB } = config.options.loginOptions;
 
-jest.setTimeout(20000);
+jest.setTimeout(30000);
 
 test("Test watch and unwatch of author", async () => {
   Login(cookieA, cookieB);
@@ -20,31 +20,31 @@ test("Test watch and unwatch of author", async () => {
 
   // test watching
   expect(author.stats?.watching).toBeFalsy();
-  console.log('Before watch', author);
+  console.log("Before watch", author);
   await watchAuthor(userId);
   author = await Author(userId);
-  console.log('After watch', author);
+  console.log("After watch", author);
   expect(author.stats?.watching).toBeTruthy();
-  
+
   // test unwatching
   expect(author.stats?.watching).toBeTruthy();
-  console.log('Before unwatch', author);
+  console.log("Before unwatch", author);
   await unwatchAuthor(userId);
   author = await Author(userId);
-  console.log('After unwatch', author);
+  console.log("After unwatch", author);
   expect(author.stats?.watching).toBeFalsy();
 
   // reset watch state
   if (watchStateBefore) await author.watchAuthor?.();
 
   // test watch self, make sure no effact
-  const self = await User()
+  const self = await User();
   expect(self.watchLink).toBeUndefined();
   expect(self.stats?.watching).toBeFalsy();
   expect(self.watchAuthor).toBeUndefined();
   expect(self.unwatchAuthor).toBeUndefined();
-  await watchAuthor(self.id)
-  await unwatchAuthor(self.id)
+  await watchAuthor(self.id);
+  await unwatchAuthor(self.id);
   expect(self.watchLink).toBeUndefined();
   expect(self.stats?.watching).toBeFalsy();
   expect(self.watchAuthor).toBeUndefined();
